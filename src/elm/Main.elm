4000935dde59e660ad8f1bfe6281ef181bd448f6
santacos <santacos_lala@hotmail.com>
2018-02-07 22:41:36 +0700

    Introduce welcome page to main app

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -12,11 +12,13 @@ main =
 
 
 -- MODEL
-type alias Model = { question: Question }
+type Page = WelcomePage | QuestionPage
+type alias Model = { question: Question, currentPage: Page}
 
 model : Model
 model = {
     question = Question "What is your name?" ["Cos", "Dog", "Green", "Red"] "Cos" Components.Question.NotAnswered
+    , currentPage = WelcomePage
   }
 
 
@@ -38,14 +40,31 @@ view model =
   div [ class "container", style [("margin-top", "30px"), ( "text-align", "center" )] ][
     div [ class "row" ][
       div [ class "col-xs-12" ][
-        div [ class "quiz" ][
-          h3 [class "question"] [text model.question.content]
-          , div [class "list-group"] <| List.map (\choice -> choiceButton choice model.question.state) model.question.choices
-          , displayResult model.question
-          , checkButton model.question.state
-        ]
+        contentView model
       ]
     ]
+  ]
+
+contentView : Model -> Html Msg
+contentView model =
+  case model.currentPage of
+    WelcomePage -> welcomeView
+    QuestionPage -> questionView model
+
+questionView : Model -> Html Msg
+questionView model =
+  div [ class "quiz" ][
+    h3 [class "question"] [text model.question.content]
+    , div [class "list-group"] <| List.map (\choice -> choiceButton choice model.question.state) model.question.choices
+    , displayResult model.question
+    , checkButton model.question.state
+  ]
+
+welcomeView : Html Msg
+welcomeView =
+  div [ class "quiz" ][
+    h3 [class "question"] [text "Welcome to SMART QUIZ SYSTEM"]
+    , button [class "check"] [text "START"]
   ]
 
 checkButton : QuestionState -> Html Msg
